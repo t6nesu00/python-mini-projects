dff0db415a2144fcd14c684e103bf325603710ad
t6nesu00 <hlw.zigri@gamil.com>
2019-05-14 05:32:07 +0300

    first program that gives year when you become 100

---
+++ characterInput.py
@@ -0,0 +1,13 @@
+from datetime import date
+
+user_name = input("What is your good name?: ")
+user_age = int(input("What is your age?: "))
+
+gap_to_be_hundred = 100 - user_age
+
+# date.today().year gives current year
+hundred_in = date.today().year + gap_to_be_hundred
+print("Hello", user_name, "you will be 100 years old in", hundred_in)
+
+
+
