faa71fec99e7228720db801fb592b0dff7827e84
t6nesu00 <hlw.zigri@gamil.com>
2019-05-14 06:14:46 +0300

    program that prints numbers less than ten from list.

---
+++ LessThanTenList.py
@@ -0,0 +1,16 @@
+# this program will create a list that has number less than 10
+
+original_list = [2, 8, 12, 24, 34, 4, 56, 43,7, 10, 9, 21, 1]
+list_final = []
+print("Here is the original list.")
+print(original_list)
+
+for numbers in original_list:
+    if numbers < 10:
+        list_final.append(numbers)
+print("Here are the numbers less than 10: ")
+print(list_final)
+
+# created by Suman Nepali, May, 2019
+
+
